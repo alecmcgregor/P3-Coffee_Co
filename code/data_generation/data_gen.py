2fd7eb7606768e4b412878d5c98b999e3ec83dcb
alecmcgregor <120531848+alecmcgregor@users.noreply.github.com>
2025-08-01 16:33:58 -0400

    Update data_gen.py
    Feature: updated to reduce factors present on generated data

--- code/data_generation/data_gen.py
+++ code/data_generation/data_gen.py
@@ -2,21 +2,9 @@ import pandas as pd
 import random
 
 df = pd.read_csv('coffee.csv')
-df.drop(columns=['Location.Country', 'Location.Altitude.Min', 'Location.Altitude.Max', 'Data.Owner', 'Data.Production.Number of bags', 'Data.Scores.Total'], inplace = True)
-regions = list(set(df['Location.Region']))
-species = list(set(df['Data.Type.Species']))
-varieties = list(set(df['Data.Type.Variety']))
-processings = list(set(df['Data.Type.Processing method']))
-colors = ['Green', 'Blue-Green', 'Bluish-Green', 'None', 'Unknown', 'Rainbow', 'Red', 'Orange-Blue']
+df.drop(columns=['Location.Country', 'Location.Region', 'Location.Altitude.Min', 'Location.Altitude.Max', 'Location.Altitude.Average', 'Year', 'Data.Owner', 'Data.Type.Species', 'Data.Type.Variety', 'Data.Type.Processing method', 'Data.Production.Bag weight', 'Data.Production.Number of bags', 'Data.Scores.Total', 'Data.Color'], inplace = True)
 rows = []
 for i in range(99010):
-    region = random.choice(regions)
-    altitude = random.randint(0, 200000)
-    year = random.randint(2010, 2025)
-    spec = random.choice(species)
-    variety = random.choice(varieties)
-    processing = random.choice(processings)
-    weight = round(random.uniform(0, 20000), 4)
     aroma = round(random.uniform(0,10), 2)
     flavor = round(random.uniform(0, 10), 2)
     aftertaste = round(random.uniform(0, 10), 2)
@@ -26,15 +14,7 @@ for i in range(99010):
     uniformity = round(random.uniform(0, 10), 2)
     sweetness = round(random.uniform(0, 10), 2)
     moisture = round(random.uniform(0, 0.30), 2)
-    color = random.choice(colors)
     rows.append({
-        "Location.Region" : region,
-        "Location.Altitude.Average" : altitude,
-        "Year" : year,
-        "Data.Type.Species" : spec,
-        "Data.Type.Variety" : variety,
-        "Data.Type.Processing method" : processing,
-        "Data.Production.Bag weight" : weight,
         "Data.Scores.Aroma" : aroma,
         "Data.Scores.Flavor" : flavor,
         "Data.Scores.Aftertaste" : aftertaste,
@@ -44,7 +24,6 @@ for i in range(99010):
         "Data.Scores.Uniformity" : uniformity,
         "Data.Scores.Sweetness" : sweetness,
         "Data.Scores.Moisture" : moisture,
-        "Data.Color" : color
     })
 
 new_df = pd.DataFrame(rows)
